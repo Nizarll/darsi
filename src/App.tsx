af6b2bc40523807574597933fc80ff7ccba4919c
nizarll <forsubsofmy@gmail.com>
2025-12-04 19:03:42 +0000

    Added a hero page as well as a header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button"
 import { ThemeProvider, useTheme } from "./components/theme-provider"
-import { ModeToggle } from "./components/mode-toggle"
+import { Hero47 } from "./components/hero47"
+import { Navbar1 } from "./components/navbar1"
+
 
 function App() {
   const { setTheme } = useTheme()
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="flex min-h-svh flex-col items-center justify-center">
-        <ModeToggle />
+        <Navbar1/>
+        <Hero47/>
       </div>
     </ThemeProvider>
   )
