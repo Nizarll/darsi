b7ba1bb4f8096de37428fa6b7ea4b876873bbc34
nizarll <forsubsofmy@gmail.com>
2025-12-19 19:08:49 +0000

    added course list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider, useTheme } from "./components/theme-provider"
-import { Hero } from "./components/hero47"
+import { Hero } from "./components/hero"
 import { Navbar1 } from "./components/navbar1"
-
+import Index from './routes';
+import Courses from './routes/courses';
 
 function App() {
-  const { setTheme } = useTheme()
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex min-h-svh flex-col items-center justify-center">
-        <Navbar1/>
-        <Hero/>
-      </div>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="flex min-h-svh flex-col items-center">
+          <div className="flex w-full lg:justify-center justify-end px-8 py-2 lg:px-0 lg:py-0">
+            <Navbar1/>
+          </div>
+          <Routes>
+            <Route path='/' element={<Index />} />
+            <Route path='/courses' element={<Courses />} />
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
